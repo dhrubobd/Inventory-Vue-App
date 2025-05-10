23656fd36bb6596045e55188c02ffdd04400a1a4
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2025-05-10 23:38:26 +0600

    Session Based Authentication Middileware is Setup

---
+++ resources/js/Pages/Login.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <h1>This is the login page</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Middleware\SessionAuthenticate;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return inertia('Home');
+});
+Route::get('/login', function () {
+    return inertia('Login');
+});
+
+Route::middleware(SessionAuthenticate::class)->group(function () {
+    Route::get('/dashboard', function () {
+        return inertia('Dashboard');
+    });
 });
