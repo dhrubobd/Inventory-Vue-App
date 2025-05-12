cc103881cd7c1e1b4473183bbd26974a528c0285
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2025-05-12 17:02:36 +0600

    User Login with Frontend is setup

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Http\Middleware\SessionAuthenticate;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return inertia('Home');
 });
-Route::get('/login', function () {
-    return inertia('Login');
-});
+Route::get('/login',[UserController::class, 'loginPage'])->name('login');
+Route::get('/logout', [UserController::class, 'userLogout'])->name('logout');
+Route::get('/registration',[UserController::class, 'registrationPage'])->name('registration');
+
+Route::post('/user-registration', [UserController::class, 'userRegistration'])->name('user.registration');
+Route::post('/user-login', [UserController::class, 'userLogin'])->name('user.login');
 
 Route::middleware(SessionAuthenticate::class)->group(function () {
-    Route::get('/dashboard', function () {
-        return inertia('Dashboard');
-    });
+    Route::get('/dashboard', [UserController::class, 'dashboardPage'])->name('dashboard');
+    
 });
